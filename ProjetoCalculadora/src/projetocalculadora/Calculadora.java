0e6f76478f96eb4c609fa59ecc8f8f55b200f7ec
Jefferson Ferreira <jeffcabelo@gmail.com>
2024-06-25 18:49:57 -0300

    Adicionado Texto na tela de calculadora

--- ProjetoCalculadora/src/projetocalculadora/Calculadora.java
+++ ProjetoCalculadora/src/projetocalculadora/Calculadora.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package projetocalculadora;
 
-/**
- *
- * @author Jefferson
- */
 public class Calculadora {
-    
+    // implementada o back end da calculadora completa
 }
